2b6576f053d68cbebedc9b535b491c613096139b
Vaibhav <96187770+born-engineer73@users.noreply.github.com>
2023-07-10 12:40:32 +0530

    Added All necessary Files

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// var scroll_position = 0;
+// var section_position = 2700;
+
+
+  
+   
+
+
+//   scroll_position = window.scrollY;
+//   console.log(scroll_position);
+//   if (scroll_position > section_position) {
+//     $(".slideimgR");
+//   }
+
+// element.addEventListener("pageshow", myFunction);
+
+// function myFunction() {
+//   $(".slideimgR");
+// }
